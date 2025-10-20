45ac1509e4619f096d89cc1f62c0825bea908ade
joanjoc <joanjoc@gmail.com>
2025-10-20 19:05:57 +0000

    Definint path correcte

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/web-test/', // aquí el nom del teu repositori a GitHub
   plugins: [react()],
 })
